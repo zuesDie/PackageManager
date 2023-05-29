b553463bc564eb6ccaa440b5bd211f0776ed8ed5
DDC <2732108670@qq.com>
2023-05-29 16:10:55 +0800

    创建工程

---
+++ Runtime/RotateSelf.cs
@@ -0,0 +1,30 @@
+//===============================
+//  功能 ：备注 
+//  作者 ：上中野辅亚瑟王 
+//  创建时间 ：2023-05-26  12-07-37 
+//  Unity版本  ：2019.4.35f1 
+//  变更时间 :  2023-05-26  12-07-37 
+//===============================
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
