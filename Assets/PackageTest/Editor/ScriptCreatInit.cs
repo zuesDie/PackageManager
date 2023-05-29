f9977ad417dcf870bdc2196403b3b45fb4bbb096
DDC <2732108670@qq.com>
2023-05-29 16:10:55 +0800

    创建工程

---
+++ Assets/PackageTest/Editor/ScriptCreatInit.cs
@@ -0,0 +1,36 @@
+﻿//===================================================
+//备    注：替换代码注释
+//===================================================
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class ScriptCreatInit : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            ////Debug.Log("strContent"+strContent);
+            string allText = "//===============================\r\n" +
+                                        "//  功能 ：备注 \r\n" +
+                                        "//  作者 ：上中野辅亚瑟王 \r\n" +
+                                        "//  创建时间 ：#CreateTime# \r\n" +
+                                        "//  Unity版本  ：#UnityVersion# \r\n" +
+                                        "//  变更时间 :  #LastSaveTime# \r\n" +
+                                        "//===============================\r\n" + "\r\n" + "\r\n" + "\r\n" + "\r\n" + "\r\n";
+            allText += File.ReadAllText(path);
+
+            allText = allText.Replace("#CreateTime#", DateTime.Now.ToString("yyyy-MM-dd  HH-mm-ss")).Replace("#UnityVersion#", Application.unityVersion).
+                Replace("#LastSaveTime#", DateTime.Now.ToString("yyyy-MM-dd  HH-mm-ss"));
+
+            File.WriteAllText(path, allText);
+            AssetDatabase.Refresh();
+        }
+    }
+}
